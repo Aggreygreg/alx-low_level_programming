7149b04a703944008cb2eada3919f6d2b37ed919
Aggreygreg <aggreygreg@gmail.com>
2022-10-14 11:50:22 -0700

    Advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retrurn: 0
+ */
+
+int main(void)
+{
+	int i = 48, j = 49;
+
+	while (i < 58)
+	{
+		while( j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
