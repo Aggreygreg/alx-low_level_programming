9c6b7d7b4fbff8f846bd7a974b1fe082a646d096
Aggreygreg <aggreygreg@gmail.com>
2022-10-24 09:39:43 -0700

    mandatory tasks

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Function that prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line
+ *
+ * @str: String passed.
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
